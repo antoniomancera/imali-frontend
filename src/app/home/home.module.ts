2fec7f4206c3b33ee2ec13a7f517abe8517dd509
Antonio Mancera <antoniomanuelmancerahevilla@gmail.com>
2023-06-23 19:08:06 +0200

    feat: creation resume component

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -2,19 +2,14 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { IonicModule } from '@ionic/angular';
 import { FormsModule } from '@angular/forms';
-import { HomePage } from './home.page';
 
+import { HomePage } from './home.page';
 import { HomePageRoutingModule } from './home-routing.module';
 import { HeaderComponent } from './header/header.component';
-
+import { ResumeComponent } from './resume/resume.component';
 
 @NgModule({
-  imports: [
-    CommonModule,
-    FormsModule,
-    IonicModule,
-    HomePageRoutingModule
-  ],
-  declarations: [HomePage, HeaderComponent]
+  imports: [CommonModule, FormsModule, IonicModule, HomePageRoutingModule],
+  declarations: [HomePage, HeaderComponent, ResumeComponent]
 })
 export class HomePageModule {}
