ccd7ff0ea94cf286a75635a799a47b02d92dc25a
antoniomancera <49141555+antoniomancera@users.noreply.github.com>
2023-06-24 22:04:49 +0200

    feat: add transloco (#1)

---
+++ src/app/shared/shared.module.ts
@@ -0,0 +1,10 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { TranslocoRootModule } from './transloco-root.module';
+
+@NgModule({
+  declarations: [],
+  imports: [CommonModule, TranslocoRootModule]
+})
+export class SharedModule {}
