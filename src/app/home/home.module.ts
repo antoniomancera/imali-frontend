cc84a3e5cabf61723bf513b94ea78aec833cc5d0
Antonio Mancera <antoniomanuelmancerahevilla@gmail.com>
2023-06-24 22:02:52 +0200

    feat: add transloco

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -7,9 +7,10 @@ import { HomePage } from './home.page';
 import { HomePageRoutingModule } from './home-routing.module';
 import { HeaderComponent } from './header/header.component';
 import { ResumeComponent } from './resume/resume.component';
+import { TranslocoRootModule } from '../shared/transloco-root.module';
 
 @NgModule({
-  imports: [CommonModule, FormsModule, IonicModule, HomePageRoutingModule],
+  imports: [CommonModule, FormsModule, IonicModule, HomePageRoutingModule, TranslocoRootModule],
   declarations: [HomePage, HeaderComponent, ResumeComponent]
 })
 export class HomePageModule {}
